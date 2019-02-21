87addfb68e2b2f53da9b4579ffe5fa79204495b2
sumanchatterjee3 <sumanchatterjee84@outlook.com>
2019-02-21 19:59:59 +0530

    commit

---
+++ hello-world/src/main/java/beans/Test.java
@@ -0,0 +1,12 @@
+package beans;
+
+public class Test {
+	
+	public Test() {
+		System.out.println("Test..Constructor");
+	}
+	
+	public void hello() {
+		System.out.println("Hello World !!");
+	}
+}
